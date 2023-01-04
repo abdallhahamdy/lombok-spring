56f5ca658488f65458584d66bce18b282c1fa59b
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-01-04 22:51:20 +0200

    create student api

---
+++ src/main/java/com/spring/lombok/controller/StudentController.java
@@ -0,0 +1,49 @@
+package com.spring.lombok.controller;
+
+import com.spring.lombok.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/all")
+    public List<Student> getStudents(){
+        return create();
+    }
+
+    private List<Student> create(){
+        List<Student> students = new ArrayList<>();
+        Student student1 = new Student();
+        student1.setId(1L);student1.setName("Eslam");
+        student1.setAddress("add_1");student1.setAge("20");
+
+        Student student2 = new Student();
+        student2.setId(1L);student2.setName("Eslam");
+        student2.setAddress("add_1");student2.setAge("20");
+
+        Student student3 = new Student();
+        student3.setId(1L);student3.setName("Eslam");
+        student3.setAddress("add_1");student3.setAge("20");
+
+        Student student4 = new Student();
+        student4.setId(1L);student4.setName("Eslam");
+        student4.setAddress("add_1");student4.setAge("20");
+
+        Student student5 = new Student();
+        student5.setId(1L);student5.setName("Eslam");
+        student5.setAddress("add_1");student5.setAge("20");
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+        return students;
+    }
+}
---
+++ src/main/java/com/spring/lombok/model/Student.java
@@ -0,0 +1,44 @@
+package com.spring.lombok.model;
+
+public class Student {
+
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
